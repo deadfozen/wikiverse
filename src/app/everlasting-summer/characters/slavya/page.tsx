c42b2c82e9cd008fd0f2f82d98d36ed6eb82bb7d
deadfozen <deadfozen430@gmail.com>
2026-06-01 23:18:39 +0500

    fix(page): удалил персонажа Славя

--- src/app/everlasting-summer/characters/slavya/page.tsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-    title: "Славя — Персонажи игры Бесконечного Лета",
-};
-import ESCharacterSlavyaClient from "./ESCharacterSlavyaClient";
-export default function Page() {
-    return <ESCharacterSlavyaClient/>;
-}
